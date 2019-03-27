17707753565bd711b56b39541bada1dedca4b201
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-27 15:43:32 -0500

    Add a category for NSString

---
+++ DdocumentsObjc/Categories/NSString+WordCount.h
@@ -0,0 +1,19 @@
+//
+//  NSString+WordCount.h
+//  DdocumentsObjc
+//
+//  Created by Ilgar Ilyasov on 3/27/19.
+//  Copyright © 2019 Fun-the-mental. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (WordCount)
+
+- (int)wordCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DdocumentsObjc/Categories/NSString+WordCount.m
@@ -0,0 +1,26 @@
+//
+//  NSString+WordCount.m
+//  DdocumentsObjc
+//
+//  Created by Ilgar Ilyasov on 3/27/19.
+//  Copyright © 2019 Fun-the-mental. All rights reserved.
+//
+
+#import "NSString+WordCount.h"
+
+@implementation NSString (WordCount)
+
+- (int)wordCount
+{
+    if ([self isEqualToString:@""]) {
+        return 0;
+    }
+    
+    NSCharacterSet *separator = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSArray *words = [self componentsSeparatedByCharactersInSet: separator];
+    NSInteger countOfWord = [words count];
+    
+    return (int)countOfWord;
+}
+
+@end
